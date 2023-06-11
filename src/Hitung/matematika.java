0dd2d6ad984803a4012394ab3c9b8bab2e617660
yofieedr666 <yofiemadridista@gmail.com>
2023-06-12 02:26:59 +0800

    Melengkapi Method matematika

--- src/Hitung/matematika.java
+++ src/Hitung/matematika.java
@@ -13,5 +13,14 @@ public class matematika {
     public double setPenjumlahan(){
         return bil1 + bil2;
     }
-    
+     public double setPengurangan(){
+        return bil1 - bil2;
+     }
+      public double setPerkalian(){
+        return bil1 * bil2;
+      }
+      public double setPembagian(){
+        return bil1 / bil2;
+      }
+      
 }
