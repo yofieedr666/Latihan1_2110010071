5ca76a50b9db96a1b35cc28fd26dd996e520620f
yofieedr666 <yofiemadridista@gmail.com>
2023-06-12 02:34:27 +0800

    Membuat Class MatematikaBeraksi

--- src/Hitung/matematikaBeraksi.java
+++ src/Hitung/matematikaBeraksi.java
@@ -1,14 +1,14 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Hitung;
 
-/**
- *
- * @author USER
- */
 public class matematikaBeraksi {
-    
+    public static void main(String[] args) {
+        //membuat objek
+        matematika yofie = new matematika (7,1);
+        
+        System.out.println("Hasil Penjumlahan : " + yofie.setPenjumlahan());
+        System.out.println("Hasil Pengurangan : " + yofie.setPengurangan());
+        System.out.println("Hasil Perkalian   : " + yofie.setPerkalian());
+        System.out.println("Hasil Pembagian   : " + yofie.setPembagian());
+    }
 }
